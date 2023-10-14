b97d99fcba1b60347c5f7ad8105c9859f74e74be
sudulge <hhs0888@naver.com>
2023-10-14 18:11:24 +0900

    2p 추가, 쪼개지는 운석 추가

--- main.py
+++ main.py
@@ -4,31 +4,21 @@ import time
 
 WINDOW_WIDTH = 1680
 WINDOW_HEIGHT = 1050
+P1_WIDTH = 840
+P2_width = 1680
 
 FPS = 60
 
-# def main():
-#     pygame.init()
-#     # logo = pygame.image.load("logo32x32.png")
-#     # pygame.display.set_icon(logo)
-#     pygame.display.set_caption("minimal program")
-
-#     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-#     running = True
-
-#     while running:
-
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
+rocks = pygame.sprite.Group()
 
 class Fighter(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, border_left, border_right):
         super(Fighter, self).__init__()
         self.image = pygame.image.load('src/fighter.png')
+        self.border_left = border_left
+        self.border_right = border_right
         self.rect = self.image.get_rect()
-        self.rect.x = int(WINDOW_WIDTH / 2)
+        self.rect.x = int((self.border_right + self.border_left) / 2)
         self.rect.y = WINDOW_HEIGHT - self.rect.height
         self.dx = 0
         self.dy = 0
@@ -37,10 +27,10 @@ class Fighter(pygame.sprite.Sprite):
         self.rect.x += self.dx
         self.rect.y += self.dy
 
-        if self.rect.x < 0 or self.rect.x + self.rect.width > WINDOW_WIDTH:
+        if self.rect.x < self.border_left or self.rect.x + self.rect.width > self.border_right:
             self.rect.x -= self.dx
         
-        if self.rect.y < 0 or self.rect.y + self.rect.height > WINDOW_WIDTH:
+        if self.rect.y < 0 or self.rect.y + self.rect.height > WINDOW_HEIGHT:
             self.rect.y -= self.dy
     
     def draw(self, screen):
@@ -93,7 +83,21 @@ class Rock(pygame.sprite.Sprite):
     def out_of_screen(self):
         if self.rect.y > WINDOW_HEIGHT:
             return True
-        
+
+class DefaultRock(Rock):
+    def __init__(self, xpos, ypos, speed):
+        super().__init__(xpos, ypos, speed)
+
+class SplitRock(Rock):
+    def __init__(self, xpos, ypos, speed):
+        super().__init__(xpos, ypos, speed)
+
+    def split(self):
+        speed = self.speed + 10
+        rock1 = DefaultRock(self.rect.x - 50, self.rect.y + 5, speed)
+        rock2 = DefaultRock(self.rect.x + 50, self.rect.y + 5, speed)
+        rocks.add(rock1)
+        rocks.add(rock2)
 
 def draw_text(text, font, surface, x, y, main_color):
     text_obj = font.render(text, True, main_color)
@@ -110,23 +114,24 @@ def occur_explosion(surface, x, y):
     explosion_rect.y = y
     surface.blit(explosion_image, explosion_rect)
     
-    explosion_sounds = [f'src/explosion{i:02d}.wav' for i in range(1, 4)]
-    explosion_sound = pygame.mixer.Sound(random.choice(explosion_sounds))
-    explosion_sound.play()
+    # explosion_sounds = [f'src/explosion{i:02d}.wav' for i in range(1, 4)]
+    # explosion_sound = pygame.mixer.Sound(random.choice(explosion_sounds))
+    # explosion_sound.play()
 
 def game_loop():
     default_font = pygame.font.Font('src/NanumGothic.ttf', 28)
     background_image = pygame.image.load('src/background.png')
     gameover_sound = pygame.mixer.Sound('src/gameover.wav')
-    pygame.mixer.music.load('src/music.wav')
-    pygame.mixer.music.play(-1)
+    # pygame.mixer.music.load('src/music.wav')
+    # pygame.mixer.music.play(-1)
     fps_clock = pygame.time.Clock()
 
-    fighter = Fighter()
+    p1_fighter = Fighter(0, 840)
+    p2_fighter = Fighter(840, 1680)
     missiles = pygame.sprite.Group()
-    rocks = pygame.sprite.Group()
+    # rocks = pygame.sprite.Group()
 
-    occur_prob = 40
+    # occur_prob = 40
     shot_count = 0
     count_missed = 0
 
@@ -138,36 +143,59 @@ def game_loop():
                 running = False
             
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    fighter.dx -= 5
-                    print(background_image.get_rect())
+                if event.key == pygame.K_a:
+                    p1_fighter.dx -= 10
+                elif event.key == pygame.K_d:
+                    p1_fighter.dx += 10
+                elif event.key == pygame.K_w:
+                    p1_fighter.dy -= 10
+                elif event.key == pygame.K_s:
+                    p1_fighter.dy += 10
+                elif event.key == pygame.K_LEFT:
+                    p2_fighter.dx -= 10
                 elif event.key == pygame.K_RIGHT:
-                    fighter.dx += 5
+                    p2_fighter.dx += 10
                 elif event.key == pygame.K_UP:
-                    fighter.dy -= 5
+                    p2_fighter.dy -= 10
                 elif event.key == pygame.K_DOWN:
-                    fighter.dy += 5
+                    p2_fighter.dy += 10
                 elif event.key == pygame.K_SPACE:
-                    missile = Missile(fighter.rect.centerx, fighter.rect.y, 10)
+                    missile = Missile(p1_fighter.rect.centerx, p1_fighter.rect.y, 10)
+                    missile.launch()
+                    missiles.add(missile)
+                elif event.key == pygame.K_RCTRL:
+                    missile = Missile(p2_fighter.rect.centerx, p2_fighter.rect.y, 10)
                     missile.launch()
                     missiles.add(missile)
 
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    fighter.dx = 0
+                if event.key == pygame.K_a or event.key == pygame.K_d:
+                    p1_fighter.dx = 0
+                elif event.key == pygame.K_w or event.key == pygame.K_s:
+                    p1_fighter.dy = 0
+                elif event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    p2_fighter.dx = 0
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    fighter.dy = 0
+                    p2_fighter.dy = 0
 
         screen.blit(background_image, background_image.get_rect())
 
-        occur_of_rocks = 1 + int(shot_count / 300)
+        occur_of_default_rocks = 3 + int(shot_count / 10)
+        occur_of_split_rocks = occur_of_default_rocks - 1
         min_rock_speed = 1 + int(shot_count / 200)
         max_rock_speed = 1 + int(shot_count / 100)
 
-        if random.randint(1, occur_prob) == 1:
-            for i in range(occur_of_rocks):
+        probablity_num = random.randint(1, 100)
+
+        if probablity_num > 99:
+            for i in range(occur_of_default_rocks):
                 speed = random.randint(min_rock_speed, max_rock_speed)
-                rock = Rock(random.randint(0, WINDOW_WIDTH - 30), 0, speed)
+                rock = DefaultRock(random.randint(0, WINDOW_WIDTH - 30), 0, speed)
+                rocks.add(rock)
+        elif probablity_num > 98:
+            for i in range(occur_of_split_rocks):
+                speed = random.randint(min_rock_speed, max_rock_speed)
+                rock = SplitRock(random.randint(0, WINDOW_WIDTH - 30), 0, speed)
                 rocks.add(rock)
         
         draw_text(f'파괴한 운석: {shot_count}', default_font, screen, 100, 20, (255, 255, 255))
@@ -176,6 +204,8 @@ def game_loop():
         for missile in missiles:
             rock = missile.collide(rocks)
             if rock:
+                if type(rock).__name__ == 'SplitRock':
+                    rock.split()
                 missile.kill()
                 rock.kill()
                 occur_explosion(screen, rock.rect.x, rock.rect.y)
@@ -190,15 +220,25 @@ def game_loop():
         rocks.draw(screen)
         missiles.update()
         missiles.draw(screen)
-        fighter.update()
-        fighter.draw(screen)
+        p1_fighter.update()
+        p2_fighter.update()
+        p1_fighter.draw(screen)
+        p2_fighter.draw(screen)
         pygame.display.flip()
 
-        if fighter.collide(rocks) or count_missed >= 3:
+        if p1_fighter.collide(rocks) or count_missed >= 3:
             pygame.mixer_music.stop()
-            occur_explosion(screen, fighter.rect.x, fighter.rect.y)
+            occur_explosion(screen, p1_fighter.rect.x, p1_fighter.rect.y)
             pygame.display.update()
-            gameover_sound.play()
+            # gameover_sound.play()
+            time.sleep(1)
+            running = False
+
+        if p2_fighter.collide(rocks) or count_missed >= 3:
+            pygame.mixer_music.stop()
+            occur_explosion(screen, p2_fighter.rect.x, p2_fighter.rect.y)
+            pygame.display.update()
+            # gameover_sound.play()
             time.sleep(1)
             running = False
 
